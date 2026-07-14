62be6b5b92d86224297779d880df2f8171b0eec2
nautrw <nautrw@proton.me>
2026-07-14 13:35:54 -0400

    clean up floor initialization

--- fluttering-bird.py
+++ fluttering-bird.py
@@ -1,10 +1,14 @@
+from math import floor
+
 import pygame as pg
 from pygame.locals import *
+
 from scripts.bird import Bird
 from scripts.floor import Floor
 
+
 class Game:
-    def __init__(self, width: int = 400, height = 800, fps: int = 60):
+    def __init__(self, width: int = 400, height=800, fps: int = 60):
         pg.init()
 
         self.width = width
@@ -16,9 +20,12 @@ class Game:
         self.clock = pg.time.Clock()
         self.dt = 0
 
-        self.bird = Bird(self.width * .15, self.height / 2)
+        self.bird = Bird(self.width * 0.15, self.height / 2)
 
-        self.floor = pg.sprite.Group(Floor(0, 700), Floor(400, 700))
+        # The floors are actually 2 floor sprites that keep wrapping behind
+        # each other, so as to create the effect that its infinite
+        floor_y = 688
+        self.floor = pg.sprite.Group(Floor(0, floor_y), Floor(self.width, floor_y))
 
     def run(self):
         while self.running:
@@ -34,11 +41,12 @@ class Game:
             self.bird.update(self.dt)
             self.bird.draw(self.screen)
 
-            self.floor.update()
+            self.floor.update(self.width)
             self.floor.draw(self.screen)
 
             pg.display.flip()
             self.dt = self.clock.tick(self.fps) / 1000
+
 
 if __name__ == "__main__":
     Game().run()
--- scripts/floor.py
+++ scripts/floor.py
@@ -13,11 +13,14 @@ class Floor(pg.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.topleft = (x, y)
 
-    def update(self):
+    def update(self, screen_width: int | float):
         self.rect.x -= 2
 
         if self.rect.right <= 0:
-            self.rect.x = 400
+            # The floors are actually 2 floor sprites that keep wrapping behind
+            # each other, so as to create the effect that its infinite
+            # this is the part that makes it wrap
+            self.rect.x = screen_width
 
     def draw(self, screen: pg.Surface):
         screen.blit(self.image, self.rect)
