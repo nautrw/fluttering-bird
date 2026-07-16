eae20da9a29291640198cdcf02aac20b9e77a61e
nautrw <nautrw@proton.me>
2026-07-15 23:50:56 -0400

    use pygame_gui for title

--- scenes/title.py
+++ scenes/title.py
@@ -1,6 +1,4 @@
-from utils import load_font
 import pygame as pg
-from pygame import freetype as ft
 from core.scene_manager import Scene, SceneManager
 from scenes.main_game import MainGameScene
 import pygame_gui as pgui
@@ -8,20 +6,15 @@ import pygame_gui as pgui
 class TitleScene(Scene):
     def __init__(self, manager: SceneManager):
         super().__init__(manager)
-        ft.init()
 
-        self.font = load_font("Boxy-Bold")
+        title_rect = pg.Rect(0, 0, 200, 100)
+        self.title = pgui.elements.UILabel(title_rect, "Fluttering Bird", self.manager.ui_manager, anchors = {'centerx': 'centerx'})
 
         play_button_rect = pg.Rect(0, 0, 200, 100)
         self.play_button = pgui.elements.UIButton(play_button_rect, "Play", self.manager.ui_manager, anchors={'center': 'center'})
 
     def render(self):
         self.manager.screen.fill("black")
-
-        text, rect = self.font.render("Fluttering Bird", "white", size=32)
-        rect.center = (self.manager.screen.width // 2, self.manager.screen.height * .25)
-        self.manager.screen.blit(text, rect)
-
         self.manager.ui_manager.draw_ui(self.manager.screen)
 
     def handle_events(self, events: list[pg.event.Event]):
