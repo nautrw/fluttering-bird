e09cfb3225fa4d45dd57ec40ec89255bda516a2e
nautrw <nautrw@proton.me>
2026-07-15 23:06:02 -0400

    clean up game.py

--- core/game.py
+++ core/game.py
@@ -26,57 +26,11 @@ class Game:
         self.clock = pg.time.Clock()
         self.dt = 0
 
-        self.bird = Bird(self.width * 0.15, self.height / 2)
-
-        # The floors are actually 2 floor sprites that keep wrapping behind
-        # each other, so as to create the effect that its infinite
-        floor_height = load_sprite("floor").get_rect().height
-        floor_y = self.height - floor_height
-        self.floor = pg.sprite.Group(Floor(0, floor_y), Floor(self.width, floor_y))
-        
-        self.pipes = pg.sprite.Group()
-        self.pipe_spawn_dt_count = 1.5
-        self.pipe_spawn_dt_timer = 0
-        self.pipe_min_y = int(load_sprite('pipe').get_rect().height * .35)
-        self.pipe_max_y = floor_y - 150
-
-        self.paused = False
-
         self.ui_manager = pgui.UIManager((self.width, self.height))
         self.manager = SceneManager(self.screen, self.ui_manager)
         self.manager.go_to(TitleScene(self.manager))
 
     def run(self):
-        # while self.running:
-        #     self.screen.fill((0, 0, 0))
-
-            #
-            # if self.pipe_spawn_dt_timer >= self.pipe_spawn_dt_count:
-            #     pipe_y = random.randint(self.pipe_min_y, self.pipe_max_y)
-            #     pipe = Pipe(self.width, pipe_y)
-            #     self.pipes.add(pipe)
-            #     self.pipe_spawn_dt_timer = 0
-
-            # self.bird.draw(self.screen)
-            # self.pipes.draw(self.screen)
-            # self.floor.draw(self.screen)
-
-            # if not self.paused:
-            #     self.bird.update(self.dt)
-            #     self.floor.update(self.dt)
-            #     self.pipes.update(self.dt)
-
-            # if pg.sprite.spritecollide(self.bird, self.pipes, False, pg.sprite.collide_mask): # ty:ignore
-            #     exit()
-
-            # for pipe in self.pipes:
-            #     if not pipe.passed and pipe.rect.right < self.bird.rect.centerx:
-            #         pipe.passed = True
-            #         print("score")
-
-            # pg.display.flip()
-            # self.dt = self.clock.tick(self.fps) / 1000
-            # self.pipe_spawn_dt_timer += self.dt
         while self.running: 
             if pg.event.get(QUIT):
                 exit()
