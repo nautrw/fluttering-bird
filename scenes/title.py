b002aa11c0c4cebf28a81592caa264c7c16e2066
nautrw <nautrw@proton.me>
2026-07-15 21:26:12 -0400

    add basic title screen

--- scenes/title.py
+++ scenes/title.py
@@ -1,6 +1,16 @@
+from utils import load_font
 import pygame as pg
+from pygame import freetype as ft
 from core.scene_manager import Scene
 
 class TitleScene(Scene):
-    def update(self):
-        print("update")
+    def __init__(self):
+        ft.init()
+
+        self.font = load_font("Boxy-Bold")
+
+    def render(self, screen: pg.Surface):
+        screen.fill("black")
+
+        title_pos = (screen.width // 2, screen.height * .25)
+        self.font.render_to(screen, title_pos, "Fluttering Bird", "white", size=32)
