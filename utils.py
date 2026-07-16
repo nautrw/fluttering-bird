4b8e74d5825bcd59572443e4e67f75397f301e17
nautrw <nautrw@proton.me>
2026-07-15 21:26:07 -0400

    add load font util function

--- utils.py
+++ utils.py
@@ -1,6 +1,11 @@
 import os
 import pygame as pg
+import pygame.freetype as ft
 
 def load_sprite(name: str) -> pg.Surface:
     path = os.path.join("assets", "sprites", f"{name}.png")
     return pg.image.load(path).convert_alpha()
+
+def load_font(name: str) -> ft.Font:
+    path = os.path.join("assets", "fonts", f"{name}.ttf")
+    return ft.Font(path)
